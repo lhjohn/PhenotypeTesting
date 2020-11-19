b0fc6c4997f16676f38b24aad89e72a4a82fa0e0
emaprojects <71638698+emaprojects@users.noreply.github.com>
2020-11-19 16:39:58 +0000

    Update MonthlyCounts.sql
    fix monthly counts

--- inst/sql/sql_server/MonthlyCounts.sql
+++ inst/sql/sql_server/MonthlyCounts.sql
@@ -1,6 +1,6 @@
 select cohort_definition_id, 
-       cast(dateadd(month,datediff(month,datefromparts(2016, 12, 01),cohort_start_date),datefromparts(2016, 12, 01)) as date) as index_month,
-	     count(distinct subject_id) as count_persons
- from @cohort_database_schema.@cohort_table
+       DATEFROMPARTS(YEAR(cohort_start_date), MONTH(cohort_start_date), 1) as index_month,
+       count(distinct subject_id) as count_persons
+from @cohort_database_schema.@cohort_table
 where cohort_start_date >= datefromparts(2016, 12, 01)
-group by cohort_definition_id, cast(dateadd(month,datediff(month,datefromparts(2016, 12, 01),cohort_start_date),datefromparts(2016, 12, 01)) as date);
+group by cohort_definition_id, DATEFROMPARTS(YEAR(cohort_start_date), MONTH(cohort_start_date), 1);
