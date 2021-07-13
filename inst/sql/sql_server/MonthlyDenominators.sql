2c80623158d53b92501c4515cb4474272bebdd27
Markov <emarkov@woksapp13p.rxcorp.com>
2021-07-13 07:29:21 -0400

    Fixed denominators

--- inst/sql/sql_server/MonthlyDenominators.sql
+++ inst/sql/sql_server/MonthlyDenominators.sql
@@ -1,5 +1,5 @@
-select DATEFROMPARTS(YEAR(visit_start_date), MONTH(visit_start_date), 1) as visit_month,
-	     count(distinct subject_id) as count_persons
+select DATEFROMPARTS(YEAR(visit_start_date), MONTH(visit_start_date), 1) as index_month,
+	     count(distinct person_id) as count_persons
 from @cdm_database_schema.visit_occurrence
 where visit_start_date >= datefromparts(2016, 12, 01)
 group by DATEFROMPARTS(YEAR(visit_start_date), MONTH(visit_start_date), 1);
